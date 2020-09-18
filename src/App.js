3c1ba45c2460069b6ea1e37e1765e89d0c780429
Séyhan A. Cileli <52051065+HanSupreme@users.noreply.github.com>
2020-09-17 21:54:30 -0400

    added sign in form, register form, face detection now works and a box will appear around face

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Logo from "./components/Logo/Logo";
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import SignIn from "./components/SignIn/SignIn";
+import Register from "./components/Register/Register";
 
 const app = new Clarifai.App({
     apiKey: '23037553394245a2bbf8a1cfb88deaca'
@@ -29,8 +31,28 @@ class App extends Component {
         super(props);
         this.state = {
             input: '',
-            imageUrl: ''
+            imageUrl: '',
+            box: {},
+            route: 'signin',
+            isSignedIn: false
         }
+    }
+
+    calculateFaceLocation = (data) => {
+        const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+        const image = document.getElementById('inputImage');
+        const width = Number(image.width);
+        const height = Number(image.height);
+        return {
+            leftCol: clarifaiFace.left_col * width,
+            topRow: clarifaiFace.top_row * height,
+            rightCol: width - (clarifaiFace.right_col * width),
+            bottomRow: height - (clarifaiFace.bottom_row * height)
+        }
+    }
+
+    displayFaceBox = (box) => {
+        this.setState({box: box})
     }
 
     onInputChange = (event) => {
@@ -43,29 +65,39 @@ class App extends Component {
         this.setState({imageUrl: this.state.input});
         app.models
             .predict(
-            Clarifai.FACE_DETECT_MODEL,
+                Clarifai.FACE_DETECT_MODEL,
                 this.state.input)
-            .then(
-                function (response) {
-                    //do something
-                },
-                function (err) {
-                    //there was an error for example
-                }
-            )
+            .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+            .catch(err => console.log(err))
     }
 
+    onRouteChange = (route) => {
+        if (route === 'signout') {
+            this.setState({isSignedIn: false})
+        } else if (route === 'home') {
+            this.setState({isSignedIn: true})
+        } this.setState({route: route})}
+
     render() {
+        const {isSignedIn, imageUrl, route, box} = this.state;
+
         return (
             <div className="App">
-                <Particles className='particles'
-                           params={particlesOptions}/>
-                <Navigation/>
-                <Logo/>
-                <Rank/>
-                <ImageLinkForm onInputChange={this.onInputChange}
-                               onButtonSubmit={this.onButtonSubmit}/>
-                <FaceRecognition imageUrl={this.state.imageUrl}/>
+                <Particles className='particles' params={particlesOptions}/>
+                <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+                {route === 'home'
+                    ? <div>
+                        <Logo/>
+                        <Rank/>
+                        <ImageLinkForm
+                            onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+                        <FaceRecognition box={box} imageUrl={imageUrl}/>
+                    </div>
+                    : (
+                        route === 'signin'
+                            ? <SignIn onRouteChange={this.onRouteChange}/>
+                            : <Register onRouteChange={this.onRouteChange}/>
+                    )}
             </div>
         )
     }
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React from "react"
+import './FaceRecognition.css'
 
-const FaceRecognition = ({imageUrl}) => {
+const FaceRecognition = ({imageUrl, box}) => {
     return (
         <div className='center ma'>
             <div className='absolute mt'>
-            <img width='500px' height='auto'
-                alt='' src={imageUrl}/>
+                <img className='ma3' id='inputImage' alt='' src={imageUrl} width='500px' heigh='auto'/>
+                <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
             </div>
         </div>
     )
 }
 
-export default FaceRecognition;
+export default FaceRecognition
