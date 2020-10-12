d2b1df624d218984b1169fa8c72bff464485aa2a
Séyhan A. Cileli <52051065+HanSupreme@users.noreply.github.com>
2020-10-12 18:14:26 -0400

    upgraded file changes, added background address that is deployed on heroku

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ class App extends Component {
     };
 
     calculateFaceLocation = (data) => {
-        const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+        const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
         const image = document.getElementById('inputImage');
         const width = Number(image.width);
         const height = Number(image.height);
@@ -80,7 +80,7 @@ class App extends Component {
         console.log('click')
         this.setState({imageUrl: this.state.input});
 
-        fetch('http://localhost:3000/imageurl', {
+        fetch('https://still-everglades-31089.herokuapp.com/imageurl', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -91,7 +91,7 @@ class App extends Component {
             .then(response => {
                 console.log('hi', response)
                 if (response) {
-                    fetch('http://localhost:3000/image', {
+                    fetch('https://still-everglades-31089.herokuapp.com/image', {
                         method: 'put',
                         headers: {'Content-Type': 'application/json'},
                         body: JSON.stringify({
