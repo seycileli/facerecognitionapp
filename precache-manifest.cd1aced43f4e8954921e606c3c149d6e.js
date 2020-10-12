0dd1084fec7eac6ab3c28b68277349eba1eab941
Séyhan A. Cileli <52051065+HanSupreme@users.noreply.github.com>
2020-10-12 18:17:35 -0400

    Updates

---
+++ precache-manifest.cd1aced43f4e8954921e606c3c149d6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a69aa494351a616209944615342aeb43",
+    "url": "/facerecognitionapp/index.html"
+  },
+  {
+    "revision": "c83cb80858f8d13436d2",
+    "url": "/facerecognitionapp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c7a2756f999e4bf858f2",
+    "url": "/facerecognitionapp/static/css/main.bcb41896.chunk.css"
+  },
+  {
+    "revision": "c83cb80858f8d13436d2",
+    "url": "/facerecognitionapp/static/js/2.c4331954.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facerecognitionapp/static/js/2.c4331954.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7a2756f999e4bf858f2",
+    "url": "/facerecognitionapp/static/js/main.3dd9e020.chunk.js"
+  },
+  {
+    "revision": "8e674af0dca338f7d13c",
+    "url": "/facerecognitionapp/static/js/runtime-main.e6136c2d.js"
+  }
+]);
